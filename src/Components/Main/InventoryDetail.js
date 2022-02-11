62407e7f46ea3760bd3412f6c1a2dbed0bc0fc3d
Ryan Flitcroft <ryanflitcroft@ymail.com>
2022-02-11 14:19:58 -0800

    State set for item based on url params in InventoryDetail.

--- src/Components/Main/InventoryDetail.js
+++ src/Components/Main/InventoryDetail.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { getInventoryItem } from '../../services/fetch-utils';
 
 export default function InventoryDetail() {
+  const params = useParams();
+  console.log('||params', params);
+
+  useEffect(() => {
+    async function getData() {
+      await getInventoryItem(params.id);
+    }
+  }, []);
+
   return (
     <>
-    
+
     </>
   );
 }
