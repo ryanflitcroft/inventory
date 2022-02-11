8f87b6f3a87c6421c25e67e3f0720d3f50d5fcd1
Ryan Flitcroft <ryanflitcroft@ymail.com>
2022-02-11 14:11:28 -0800

    Link setup for InventoryItem component elements. Css styling.

--- src/Components/Main/InventoryItem.js
+++ src/Components/Main/InventoryItem.js
@@ -1,17 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function InventoryItem({ item }) {
   console.log('||item ', item);
   return (
     <>
-      <li>
-        <span>
-          {item.item}
-        </span>
-        <span>
-          {item.quantity}
-        </span>
-      </li>
+      <Link to={`/inventory-detail/${item.id}`}>
+        <li>
+          <span>
+            {item.item}
+          </span>
+          <span>
+            {item.quantity}
+          </span>
+        </li>      
+      </Link>
+
     </>
   );
 }
